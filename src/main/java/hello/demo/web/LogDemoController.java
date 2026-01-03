24496ed15685e0b02f527b0728a2df2e32c3207a
이용민 <bacaren21@naver.com>
2026-01-03 19:50:51 +0900

    10강 수강

---
+++ src/main/java/hello/demo/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package hello.demo.web;
+
+import hello.demo.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor // 롬복으로 생성자 자동 주입
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
---
+++ src/test/java/hello/demo/scope/PrototypeTest.java
@@ -0,0 +1,45 @@
+package hello.demo.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(prototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        prototypeBean prototypeBean1 = ac.getBean(prototypeBean.class);
+
+        System.out.println("find prototypeBean2");
+        prototypeBean prototypeBean2 = ac.getBean(prototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class prototypeBean{
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destory(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
